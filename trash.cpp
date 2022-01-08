aab2a470b6707b8f061b6256fc1bf00576dc2118
BACH <xuanbachvo7@gmail.com>
2022-01-08 15:03:19 -0800

    added file

---
+++ trash.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){f
+    int n;
+    string c;
+    cin >> n;
+    getline(cin, c);
+
+    cout << n << endl;
+    cout << c;
+
+
+return 0;
+}
