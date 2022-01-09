b9f7af5f58489b3496c358bfca1ba238cfadf744
BACH <xuanbachvo7@gmail.com>
2022-01-08 22:03:38 -0800

    Just before adding card type

--- cCardValidator.cpp
+++ cCardValidator.cpp
@@ -5,26 +5,42 @@ using namespace std;
 
 int main(){
     int size = 16;
-    char arr[] = "4388576018402626";                                  //checks real list size
+    char arr[] = "4388576018410707";                                  //checks real list size
     /*
     cout << "Enter Card Number: ";
     cin.getline(arr, size, '\n');
     size = strlen(arr);
     */
-   int temp =0;
+   int tempSum =0;
    int sum =0;
     for (int i = size-2; i >= 0; i-=2)
     {
         sum = (arr[i] -48) * 2;
         //cout << '\t' << "arr[i]:" << arr[i] << "\t i:" << i << endl;
-        cout << "sum:" << sum << '\n';
+        //cout << "sum:" << sum << '\n';
         if (sum >= 10)
         {
-            temp = (sum/ 10) + (sum% 10);
-            arr[i] = temp;
-            cout << arr[i];
+            tempSum = (sum/ 10) + (sum% 10);
+            cout << "temp:" << tempSum << endl;
+            arr[i] = tempSum +48;
+            cout << arr[i] << endl;
         }
+        else
+            arr[i] = sum +48;
     }
+    int total = 0;
+    for (int k = 0; k < size; k++)
+    {
+        cout << arr[k] << ", ";
+        total += arr[k] -48;
+    }
+    cout << "\ntotal:" << total << endl;
+    if (total%10 == 0)
+    {
+        cout << "Card is Valid!";
+    }
+    else cout << "Card is InValid...";
+    
 }
 //sumE
 //sumO
